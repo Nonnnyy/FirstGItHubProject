6e1f295b7d0007e8c6cd4d56368db6a9f0176387
nonso <nonnny07@gmail.com>
2022-08-03 12:22:17 +0100

    first commit

---
+++ src/main/java/com/nonny/org/FirstGitHubProject.java
@@ -0,0 +1,13 @@
+package com.nonny.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGitHubProject {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstGitHubProject.class, args);
+	}
+
+}
